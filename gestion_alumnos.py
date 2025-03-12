4b49fe64af67378e9aa9651e05912550215d23ae
blaze-darck <paredesgabriel784@gmail.com>
2025-03-12 09:55:50 -0400

    Mis codigos comentados pero tarde

--- gestion_alumnos.py
+++ gestion_alumnos.py
@@ -1,21 +1,22 @@
-import json
 
-def cargar_alumnos(ruta_archivo):
+import json  #Importando nuestro archivo Json 
+ 
+def cargar_alumnos(ruta_archivo): #Creando una funcion para cargar los datos de los alumnos
     with open(ruta_archivo, 'r') as archivo:
         return json.load(archivo)
                             
-def buscar_alumno(alumnos, nombre):
+def buscar_alumno(alumnos, nombre): #Segunda funcion para buscar alumnos del archivo json
     for alumno in alumnos:
-        if alumno['nombre'].lower() == nombre.lower():
+        if alumno['nombre'].lower() == nombre.lower(): #Si se pone mayusculas o minusculas igual se busque 
             return alumno
-    return None
+    return None 
 
-if __name__ == "__main__":
-    ruta = "alumnos.json"
+if __name__ == "__main__":   #Hacer correr nuestor archivo 
+    ruta = "Laboratorio6/alumnos.json" #Ruta del archivo Json para la busqueda 
     alumnos = cargar_alumnos(ruta)
     nombre_buscar = input("Ingrese el nombre del alumno a buscar: ")
 
-    resultado = buscar_alumno(alumnos, nombre_buscar)
+    resultado = buscar_alumno(alumnos, nombre_buscar)  #Resultado de la busqueda
     if resultado:
         print(f"Alumno encontrado: {resultado}")
     else:
--- gestion_alumnos_test.py
+++ gestion_alumnos_test.py
@@ -1,21 +1,21 @@
-import unittest
-from gestion_alumnos import buscar_alumno
+import unittest  
+from gestion_alumnos import buscar_alumno #Del archivo gestion_alumnos se importa su metodo de buscar_alumno
 
-class TestGestionAlumnos(unittest.TestCase):
+class TestGestionAlumnos(unittest.TestCase): #Clase del archivo gestion_alumnos_test
 
-    def setUp(self):
+    def setUp(self): #Funcion de la clase TestGestionAlumnos 
         self.alumnos = [{"nombre": "Ana","edad":20,"carrera":"Ingenieria"},
                         {"nombre": "Luis","edad":22,"carrera":"Matematicas"},
-                        {"nombre": "Maria","edad":21,"carrera":"Fisica"}]
+                        {"nombre": "Maria","edad":21,"carrera":"Fisica"}]  #Datos para verificar si hay errores
 
-    def test_buscar_alumno_existente(self):
+    def test_buscar_alumno_existente(self):   #Buscador de errores 
         resultado = buscar_alumno(self.alumnos, "Ana")
         self.assertIsNotNone(resultado)
         self.assertEqual(resultado["nombre"], "Ana")
 
-    def test_buscar_alumno_inexistente(self):
+    def test_buscar_alumno_inexistente(self): #Buscador de errores
         resultado = buscar_alumno(self.alumnos, "Carlos")
         self.assertIsNone(resultado)
 
-if __name__ == "__main__":
+if __name__ == "__main__":  #Metodo principal
     unittest.main()
